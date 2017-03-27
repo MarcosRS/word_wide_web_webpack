8680496566e03e7faf1254689f8bf59242f42bc6
MarcosRS <MarcosRS16@gmail.com>
2017-03-27 10:03:59 -0400

    ultimate

--- browser/app.js
+++ browser/app.js
@@ -1,16 +1,28 @@
 
 var socket = io(window.location.origin);
 
+
+
 socket.on('connect', function () {
     console.log('I have made a persistent two-way connection to the server!');
 });
 
 
+socket.on('draw',function(payload){
 
+	console.log(payload);
+	console.log('browser drawing');
 
+	whiteboard.on('draw', function(){
+  		console.log('it is drawing!');
+	})
 
+})
 
 
 whiteboard.on('draw', function(){
-  console.log('it is drawing!')
+  
+  // socket.emit('draw',{hello: 'nathan'})
+  socket.emit('draw', {u:'i'});
+
 })
--- server.js
+++ server.js
@@ -14,13 +14,23 @@ var io = socketio(server);
 
 io.on('connection', function (socket) {
     console.log('A new client has connected!');
-    console.log(socket.id);
-    //console.log(socket, 'socket');
-    socket.send('hey')
-socket.on('disconnect', function(){
+
+	socket.on('disconnect', function(){
         console.log('WHY???')
     })
+
+    socket.on('draw', function(obj){
+    	console.log(obj);
+        console.log('Remote Drawing')
+
+  //       whiteboard.on('draw', function(){
+
+		// })
+    })
+
+     socket.emit('draw',{hello:'hi'});
 });
+
 
 
 
